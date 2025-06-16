ec81c66aeeeca0868e30fd76523201f220496be5
JKLeorio <jkdplatinum@gmail.com>
2025-06-16 13:36:21 +0600

    added permission system, password autogeneration, beta_endpoints for creating user and admin users, fixed some autoformation errors

--- db/schemas.py
+++ db/schemas.py
@@ -6,37 +6,28 @@ from .types import Role, Gender, Level
 
 
 class UserCreate(schemas.BaseUserCreate):
-    first_name: str = Field(..., min_length=1, max_length=50)
-    last_name: str = Field(..., min_length=1, max_length=50)
     email: EmailStr = Field(..., max_length=254)
     role: Role = Field(Role.STUDENT)
 
 
 class UserResponse(BaseModel):
     id: int
-    first_name: str
-    last_name: str
     email: str
+    role: Role
     is_active: bool
-    phone_number: Optional[str] = None
+    is_admin: bool
+    is_verified: bool
 
 
 class UserRegister(BaseModel):
-    first_name: str = Field(..., min_length=1, max_length=50)
-    last_name: str = Field(..., min_length=1, max_length=50)
     email: EmailStr = Field(..., max_length=254)
-    password: str = Field(..., min_length=8, max_length=128)
     role: Role = Field(
         Role.STUDENT,
-        description="User role, default is 'STUDENT'")
+        description="User role, default is 'student'")
 
+class AdminRegister(BaseModel):
+    email: EmailStr = Field(..., max_length=254)
+    role: Role = Field(
+        Role.ADMIN,
+        description="User role, default is 'admin'")
 
-class UserUpdate(BaseModel):
-    first_name: Optional[str] = Field(None, min_length=1, max_length=50)
-    last_name: Optional[str] = Field(None, min_length=1, max_length=50)
-    email: Optional[EmailStr] = Field(None, max_length=254)
-    phone_number: Optional[str] = Field(
-        None, description="User's phone number")
-    sex: Optional[Gender] = None
-    birth_date: Optional[datetime] = Field(
-        None, description="User's birth date, iso 8601 format")
