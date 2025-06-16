efb5ffa65163d40437b0025c11b92033dc3559aa
JKLeorio <jkdplatinum@gmail.com>
2025-06-16 18:19:48 +0600

    added email unique validation in cli

--- manage.py
+++ manage.py
@@ -2,7 +2,10 @@ import asyncio
 import argparse
 import sys
 
+from sqlalchemy import select
+
 from db.database import get_async_session_context, get_user_db_context
+from db.models import User
 from db.schemas import SuperAdminCreate
 from api.auth import get_user_manager_context
 
@@ -11,6 +14,9 @@ from api.auth import get_user_manager_context
 async def createsuperuser(email: str, password: str):
     async with get_async_session_context() as session:
         async with get_user_db_context(session=session) as user_db:
+            if await session.scalar(select(User).where(User.email == email)):
+                print(f'User with this email is already registered')
+                exit()
             async with get_user_manager_context(user_db=user_db) as user_manager:
                 user = await user_manager.create(
                     SuperAdminCreate(
