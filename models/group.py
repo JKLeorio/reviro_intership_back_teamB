a35423215c8611dba873c0bec7510b93cd63cc95
Kasiet2001 <mursidinovakasiet@gmail.com>
2025-06-19 15:19:49 +0600

    endpoints and schemas for models/course.py without permissions checking

--- models/group.py
+++ models/group.py
@@ -35,8 +35,8 @@ class Group(Base):
     teacher_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     teacher: Mapped["User"] = relationship(back_populates="groups_taught")
 
-    # lessons: Mapped[List["Lesson"]] = relationship(back_populates="group", cascade="all, delete-orphan")
+    lessons: Mapped[List["Lesson"]] = relationship(back_populates="group", cascade="all, delete-orphan")
 
     students: Mapped[List["User"]] = relationship(secondary="student_group_association_table",
                                                      back_populates="groups_joined")
-    payments: Mapped[list["Payment"]] = relationship(back_populates="payments")
+    payments: Mapped[list["Payment"]] = relationship(back_populates="group")
