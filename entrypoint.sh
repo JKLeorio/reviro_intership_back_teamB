b56d2fe348b94997199bf003cba2582b3e3a3642
Kasiet2001 <mursidinovakasiet@gmail.com>
2025-07-08 14:53:22 +0600

    adding superuser entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 echo "Creating tables..."
 uv run alembic upgrade head
 
+echo "Creating superuser if not exists..."
+
+uv run manage.py createsuperuser --email "$SUPERUSER_EMAIL" --password "$SUPERUSER_PASSWORD"
+
 uv run main.py
