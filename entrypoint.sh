ba91f32b8136f8a01c926d16dd78bafd7ac168f8
Kasiet2001 <mursidinovakasiet@gmail.com>
2025-07-08 11:37:05 +0600

    fixing migrations deleting commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,10 @@ set -e
 echo 'Setting up alembic directories...'
 mkdir -p alembic/versions
 
-echo 'Setting up fresh database schema...'
-uv run alembic stamp head
-
 echo "Generating migrations from current database..."
 uv run alembic revision --autogenerate -m 'deployment schema'
 
 echo "Creating tables..."
 uv run alembic upgrade head
-
 
 uv run main.py
