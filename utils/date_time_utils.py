d014b6357c367eaffaed3a1f0ee6e5aad6f79b82
JKLeorio <jkdplatinum@gmail.com>
2025-08-07 03:32:29 +0600

    added new fixtures and fixed bug with auth fixture

--- utils/date_time_utils.py
+++ utils/date_time_utils.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
+from typing import List
 
 
 def get_current_time() -> datetime:
@@ -6,3 +7,13 @@ def get_current_time() -> datetime:
     Returns the current time in UTC.
     """
     return datetime.now(timezone.utc)
+
+def get_week_start_end() -> List[datetime]:
+    current_time = get_current_time().date()
+    week_start = current_time - timedelta(
+        days=current_time.weekday()
+        )
+    week_end = week_start + timedelta(
+        days=6
+        )
+    return [week_start, week_end]
